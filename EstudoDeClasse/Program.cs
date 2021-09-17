9a9a3db100af3abe1d1023fcc514e7e257072773
Eduardo Alves <edu.alves88@outlook.com>
2021-09-17 20:11:47 -0300

    Add project files.

---
+++ EstudoDeClasse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstudoDeClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            double xA, xB, xC, yA, yB, yC;
+
+            //Entrada de dados do primeiro triângulo
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Entrada de dados do segundo triângulo
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Cálculo da área do X
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            //Cálculo da área do Y
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            //Saída do Valor cálculado
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            //Condicional de comparação de maior área
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
